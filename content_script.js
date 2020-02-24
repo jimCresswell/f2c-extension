4aad1d26e58964118176a2984cbd5c5e51b7f21b
jimCresswell <jim.cresswell@gmail.com>
2020-02-24 16:46:30 +0000

    Add a template extension

---
+++ content_script.js
@@ -0,0 +1,4 @@
+// Put all the javascript code here, that you want to execute after page load.
+var header = document.createElement('h1');
+header.textContent = "This page has a new header";
+document.body.prepend(header);
