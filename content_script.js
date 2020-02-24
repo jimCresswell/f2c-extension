0499755cfe54593056b0ca43b3442c06534faa10
jimCresswell <jim.cresswell@gmail.com>
2020-02-24 18:19:43 +0000

    Basic working browser extension replacing temperatures in Fahrenheit with temperatures in Celsius

--- content_script.js
+++ content_script.js
@@ -1,4 +1,56 @@
-// Put all the javascript code here, that you want to execute after page load.
-var header = document.createElement('h1');
-header.textContent = "This page has a new header";
-document.body.prepend(header);
+function f2c(f) {
+    return (5/9) * (f - 32);
+}
+
+// Approach taken from https://github.com/panicsteve/cloud-to-butt 
+function walk(node) {
+    var child, next;
+
+    var tagName = node.tagName ? node.tagName.toLowerCase() : "";
+    if (tagName == 'input' || tagName == 'textarea') {
+        return;
+    }
+    if (node.classList && node.classList.contains('ace_editor')) {
+        return;
+    }
+
+    switch (node.nodeType) {
+        case 1:  // Element
+        case 9:  // Document
+        case 11: // Document fragment
+            child = node.firstChild;
+            while (child) {
+                next = child.nextSibling;
+                walk(child);
+                child = next;
+            }
+            break;
+
+        case 3: // Text node
+            handleText(node);
+            break;
+    }
+}
+
+function handleText(textNode) {
+    var v = textNode.nodeValue;
+
+    /**
+     * @todo detect negative numbers
+     * @todo detect temp ranges e.g. 60-70F
+     * @todo create a unit test for string matches and results.
+     */
+    // Doesn't match ft (feet)
+    // Initial test against https://www.rhs.org.uk/advice/profile?pid=664
+    rFahrenheitText = /(\d+)\w?(?:º|degrees)?\w?F(\)?)\b/gi;
+
+    v = v.replace(rFahrenheitText, function(match, tempF, optionalClosingBrace) {
+        return `${f2c(tempF).toFixed(2)} degrees Celsius${optionalClosingBrace}`;
+    });
+
+    textNode.nodeValue = v;
+}
+
+walk(document.body);
+
+
